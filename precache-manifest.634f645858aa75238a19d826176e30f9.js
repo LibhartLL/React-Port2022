0c256d77f7bec60c15272ed34ed6437beead257c
Lindsay Libhart <104096647+LibhartLL@users.noreply.github.com>
2022-11-06 18:52:09 -0500

    deploy to git to see if images were lost

---
+++ precache-manifest.634f645858aa75238a19d826176e30f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fd39afa4d4b199c459432e4e705cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4973fcf7e81c9ac3d405",
+    "url": "/static/css/main.862a93a7.chunk.css"
+  },
+  {
+    "revision": "c93a120eed5bcafaa638",
+    "url": "/static/js/2.d89fdc56.chunk.js"
+  },
+  {
+    "revision": "4fb2ddd01bbdb0199af7f68cae9146c9",
+    "url": "/static/js/2.d89fdc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4973fcf7e81c9ac3d405",
+    "url": "/static/js/main.22086c2c.chunk.js"
+  },
+  {
+    "revision": "045929d60bd2066a80f3",
+    "url": "/static/js/runtime-main.e43e61d1.js"
+  }
+]);
